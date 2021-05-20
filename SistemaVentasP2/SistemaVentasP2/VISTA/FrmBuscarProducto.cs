3ea521fb9d0611761617f4cabe6f97e585bbbb86
Alex-Hub-cyber <gl0430032020@unab.edu.sv>
2021-05-20 10:46:46 -0500

    FiltrarProducto
    .

---
+++ SistemaVentasP2/SistemaVentasP2/VISTA/FrmBuscarProducto.cs
@@ -0,0 +1,75 @@
+﻿using SistemaVentasP2.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaVentasP2.VISTA
+{
+    public partial class FrmBuscarProducto : Form
+    {
+        public FrmBuscarProducto()
+        {
+            InitializeComponent();
+            cargarProductos();
+        }
+        void cargarProductos()
+        {
+
+            var Lista = new ClsDProducto();
+            DtgBuscarProducto.Rows.Clear();
+
+
+            foreach (var listarDatos in Lista.cargarProductoFiltro(txtBuscarProducto.Text))
+            {
+
+
+                DtgBuscarProducto.Rows.Add(listarDatos.idProducto, listarDatos.nombreProducto, listarDatos.precioProducto);
+
+            }
+        }
+
+        private void txtBuscarProducto_TextChanged(object sender, EventArgs e)
+        {
+            cargarProductos();
+        }
+
+
+        private void DtgBuscarProducto_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            String id = DtgBuscarProducto.CurrentRow.Cells[0].Value.ToString();
+            String Nombre = DtgBuscarProducto.CurrentRow.Cells[1].Value.ToString();
+            String Precio = DtgBuscarProducto.CurrentRow.Cells[2].Value.ToString();
+           
+
+
+            FrmVenta ventas = new FrmVenta();
+
+            ventas.TxtId.Text = id;
+            ventas.TxtNombreProducto.Text = Nombre;
+            ventas.TxtPrecio.Text = Precio;
+            ventas.Show();
+
+            ///// ESTE CODIGO ES PARA QUE LO ABRA EN UN SOLO OBJETO, PERO NO ME FUNCIONA ASI QUE LE DEJE .EL QUE ABRE OTRO OBJETO.
+            //FrmMenuu.frmVenta.TxtId.Text = id;
+            //FrmMenuu.frmVenta.TxtNombreProducto.Text = Nombre;
+            //FrmMenuu.frmVenta.TxtPrecio.Text = Precio;
+            //this.Show();
+            //this.Close();
+        }
+
+        private void FrmBuscarProducto_Load(object sender, EventArgs e)
+        {
+            cargarProductos();
+        }
+
+     
+    }
+    }
+
+
---
+++ SistemaVentasP2/SistemaVentasP2/VISTA/FrmVentas.cs
@@ -0,0 +1,64 @@
+﻿using SistemaVentasP2.MODEL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaVentasP2.VISTA
+{
+    public partial class FrmVenta : Form
+    {
+        public FrmVenta()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmVentas_Load(object sender, EventArgs e)
+        {
+            using (sistema_ventasEntities db = new sistema_ventasEntities())
+            {
+                try
+                {
+                    var consultacliente = db.tb_cliente.ToList();
+
+                    comboBox2.DataSource = consultacliente;
+                    comboBox2.DisplayMember = "nombreCliente";
+                    comboBox2.ValueMember = "iDCliente";
+
+                    var consultadocumento = db.tb_documento.ToList();
+
+                    comboBox1.DataSource = consultadocumento;
+                    comboBox1.DisplayMember = "nombreDocumento";
+                    comboBox1.ValueMember = "iDDocumento";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        private void BtnBuscar_Click(object sender, EventArgs e)
+        {
+            FrmBuscarProducto  buscar = new FrmBuscarProducto();
+            buscar.Show();
+        }
+
+        private void DtgDeVentas_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string id = DtgDeVentas.CurrentRow.Cells[0].Value.ToString();
+            string nombre = DtgDeVentas.CurrentRow.Cells[1].Value.ToString();
+            string precio = DtgDeVentas.CurrentRow.Cells[2].Value.ToString();
+
+
+
+            this.Close();
+
+        }
+    }
+}
